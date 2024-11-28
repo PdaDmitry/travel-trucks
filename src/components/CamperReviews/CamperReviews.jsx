46dff02eee20a23799fbfebd129f62c8c4e9e277
PdaDmitry <pda.gazcomp@gmail.com>
2024-11-28 16:18:14 +0200

    Create CamperReviews.jsx

--- src/components/CamperReviews/CamperReviews.jsx
+++ src/components/CamperReviews/CamperReviews.jsx
@@ -1,13 +1,22 @@
+import { useParams } from 'react-router-dom';
 import { CamperBookingForm } from '../CamperBookingForm/CamperBookingForm';
 import css from './CamperReviews.module.css';
+import { useSelector } from 'react-redux';
+import { selectCampersById } from '../../redux/catalog/selectors';
 
 export const CamperReviews = () => {
+  const { id } = useParams();
+  const { reviews } = useSelector(selectCampersById(id));
+
   return (
     <div>
       <div>
         <svg className={css.line}>
           <use href="/symbol-line.svg#icon-Divider-right"></use>
         </svg>
+      </div>
+      <div>
+        <div></div>
       </div>
       <CamperBookingForm />
     </div>
