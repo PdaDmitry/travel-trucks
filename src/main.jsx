67c1becb422ae2a6ee3c9a4b40b9384fb135ba55
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-12-01 23:47:25 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
@@ -16,5 +15,4 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </PersistGate>
   </Provider>
-  //  </StrictMode>
 );
